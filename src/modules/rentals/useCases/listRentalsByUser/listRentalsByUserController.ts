aa134d38c121a29f649f9d346722a4a0e007ac50
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2022-07-04 22:15:04 -0300

    feature: add method find by user

---
+++ src/modules/rentals/useCases/listRentalsByUser/listRentalsByUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListRentalsByUserUseCase } from './ListRentalsByUserUseCase';
+
+class ListRentalsByUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
+    const listRentalsByUserUsecase = container.resolve(
+      ListRentalsByUserUseCase,
+    );
+
+    const rentals = await listRentalsByUserUsecase.execute(id);
+
+    return response.json(rentals);
+  }
+}
+
+export { ListRentalsByUserController };
